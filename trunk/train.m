a1a172bd42b167adeff736a69d20d061815ee3cc
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-19 21:14:10 +0000

    restructure

---
+++ trunk/train.m
@@ -0,0 +1,10 @@
+function [classifier] = train(examples, train_categories)
+
+% examples is a  m x num_features matrix
+%    m examples [ feature for this example ]
+
+
+% classifier is the function returned 
+% that takes a feature vector  and returns
+% a classification for that image
+
