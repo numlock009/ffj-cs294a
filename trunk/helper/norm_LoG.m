8cf8d352d4af6b243e33a40177390b1b37943586
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-20 02:50:16 +0000

    updating for svm an keypoint descriptors

---
+++ trunk/helper/norm_LoG.m
@@ -0,0 +1,16 @@
+function [norm_LoG] = norm_LoG(img, scales, furthest_std)
+
+img_height = size(img,1);
+img_width = size(img,2);
+num_scales = size(scales, 1);
+
+norm_LoG = zeros(img_height,img_width, num_scales);
+
+for i = 1:num_scales
+  local_sigma = scaled_sigma(i);
+  scaled_width = floor(2*furthest_std*local_sigma + 1); % go furthest standard deviations away up or down
+  norm_LoG(:,:,i) = local_sigma*local_sigma*...
+      imfilter(img, ...
+               fspecial('log', scaled_width, local_sigma),...
+               'replicate');
+end
