f9c0458f95791d6f8123fd070494a204226c4abc
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-21 05:37:32 +0000

    so i can move the file

--- trunk/findAllKeypoints.m
+++ trunk/findAllKeypoints.m
@@ -4,11 +4,13 @@ fid = fopen(filename , 'w');
 fprintf(fid, '%d\n',size(files,1)-2);
 min1 = 1000000;
 min2 = 1000000;
-for i=3 : size(files,1)
+for i=1 : size(files,1)
+  if(~files(i).isdir)
     points = harris_corner(imread(strcat(directory , files(i).name)),0.04,1);
     min1 = min( min1 , size(points,1));
     min2 = min( min2 , size(points,2));
     fprintf(fid, '%d\t',size(points,1));
     fprintf(fid, '%d\n',size(points,2));
     write_points(fid , points);
+  end
 end
