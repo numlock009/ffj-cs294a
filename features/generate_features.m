46020e63bc09e7f0afc9e606b65fbecdda4245e1
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-22 15:03:40 +0000

    fixed major bug with generate features

--- features/generate_features.m
+++ features/generate_features.m
@@ -1,4 +1,4 @@
-function [features] = generate_features(points_files, varargin)
+function [all_features, split_features] = generate_features(points_files, varargin)
 p = inputParser;
 p.KeepUnmatched = true;
 p.addRequired('points_files', @iscell);
@@ -7,25 +7,39 @@ p.addParamValue('max_points', 100, @(x) x>0);
 p.parse(points_files, varargin{:});
 max_points = p.Results.max_points;
 
-features = [];
+all_features = [];
+split_features = {};
+index = 1;
 for fs = 1:size(points_files, 2)
   fid = fopen( points_files{fs} , 'r');
-  index = 1;
+  split_features{fs} = {};
+  image_num = 1;
   while(true)
     fname = fscanf(fid, '%s', 1);
     if(isempty(fname)), break, end;
     m = fscanf( fid , '%d' , [1 2]);
     keypoints = fscanf(fid , '%d' , m);
     keypoints = reshape(keypoints, [m(2) m(1)])';
-    for j = 1 : min(max_points, size(keypoints, 1))
-      descriptor = get_descriptors(prepare_image( fname ), varargin{:}, ...
-                                                 'pt', keypoints(j,:));
-      
-      features(index, :) = descriptor(:)'; % same as reshaping into a 1xsize1*size2 vector
-      index = index+1;                                % represents a
-                                                      % point's descriptor
+
+    j = 1;
+    n = min(max_points, size(keypoints, 1));
+    k = 1;
+    split_features{fs}{image_num} = [];
+    while( j <= size(keypoints, 1) && k <= n)
+      try
+        descriptor = get_descriptors(prepare_image( fname ), varargin{:}, ...
+                                                   'pt', keypoints(j,:));
+      catch
+        j = j + 1;
+        continue;
+      end
+      all_features(index, :) = descriptor(:)'; % same as reshaping into a 1xsize1*size2 vector
+      split_features{fs}{image_num}(k, :) = all_features(index, :);                                               
+      index = index+1;                     % represents a
+      j = j + 1;                           % point's descriptor
+      k = k + 1;
     end
-      % represents a single image;
+    image_num = image_num + 1;
   end
   fclose(fid);
 end
