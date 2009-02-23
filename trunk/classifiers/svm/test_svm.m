9371b5816f4631e96e35f7f18bd51b1d77d6d067
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-23 15:20:07 +0000

    error outputing added

--- trunk/classifiers/svm/test_svm.m
+++ trunk/classifiers/svm/test_svm.m
@@ -1,4 +1,19 @@
-function test_svm(data, categories, paramfile, testfile, predictfile)
+function [classification_error, output] = test_svm(data, categories, paramfile, testfile, predictfile)
 option = svmlopt('ExecPath', './classifiers/svm/');
 svmlwrite( testfile , data , categories );
-svm_classify(option, testfile , paramfile , predictfile );
+svm_classify(option, testfile , paramfile , predictfile);
+
+output = zeros(size(data,1), 1);
+fid = fopen(predictfile, 'r');
+i = 0;
+while(true)
+  x = fscanf(fid, '%f', 1);
+  if(isempty(x)), break, end
+  i = i + 1;
+  output(i) = x;
+end
+fclose(fid);
+
+sgn = @(x)((x > 0) - (x <= 0)); % like the sign function but takes zero
+                                % to -1 and not 0;
+classification_error = err(sgn(output), categories);
