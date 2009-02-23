0928d011d2df01fe6d98b6d396801057bedaded6
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-23 18:01:55 +0000

    do_conv fixed to use the standard matlab call

--- keypoints/do_conv.m
+++ keypoints/do_conv.m
@@ -1,9 +1,6 @@
 function [ result ] = do_conv( image , def_kernel )
 FFTim = fft2(image);
-
-kernel=zeros(size(image));
-kernel( 1: size(def_kernel,1) , 1:size(def_kernel ,2)) = def_kernel;
-FFTkernel = fft2(kernel);
+FFTkernel = fft2(def_kernel, size(image, 1), size(image, 2));
 
 % apply the kernel and check out the result
 FFTresult = FFTim .* FFTkernel;
