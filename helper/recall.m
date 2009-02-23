03d37c161c493decab73968058a05af0c95a826c
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-23 14:17:29 +0000

    fixed the precision and recall functions

--- helper/recall.m
+++ helper/recall.m
@@ -1,4 +1,4 @@
 function R = recall(classification, actual, pos, neg)
-tp = sum(classification == actual && classification == pos);
-fn = sum(classification ~= actual && classification == neg);
+tp = sum(classification == actual & classification == pos);
+fn = sum(classification ~= actual & classification == neg);
 R = tp/(tp+fn);
