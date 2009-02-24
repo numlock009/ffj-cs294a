178f5d79be51440bd516db8da34461906d329685
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-24 02:05:44 +0000

    again fixed segment image helper

--- trunk/helper/segment_image.m
+++ trunk/helper/segment_image.m
@@ -1,4 +1,4 @@
-function segment_image(directory, final_directory, height, width)
+function segment_image(directory, final_directory, width, height)
 files = dir(directory)
 for i= 1:size(files,1)
   if(~files(i).isdir)
@@ -6,7 +6,7 @@ for i= 1:size(files,1)
     [h, w, c] = size(img);
     for row =  1:height:(h-height)
       for col = 1:width:(w-width)
-        im = imcrop(img, [col, row, height-1, width-1]);
+        im = imcrop(img, [row, col, width-1, height-1]);
         imwrite(im, strcat(final_directory, '/', 'cropped_', int2str(col), 'x', int2str(row),'_', files(i).name), ...
                 'Quality', 100);
       end
