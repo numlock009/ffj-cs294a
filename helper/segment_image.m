cb7c487873961f4d0b8a73ec6d7a4b185f88d773
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-24 04:23:59 +0000

    fixing segment_image again...

--- helper/segment_image.m
+++ helper/segment_image.m
@@ -3,9 +3,9 @@ files = dir(directory)
 for i= 1:size(files,1)
   if(~files(i).isdir)
     img = imread(strcat(directory, '/', files(i).name));
-    [h, w, c] = size(img)
-    for row =  1:height:(h-height)
-      for col = 1:width:(w-width)
+    [h, w, c] = size(img);
+    for row = 1:height:(h-height+1)
+      for col = 1:width:(w-width+1)
         im = imcrop(img, [col, row, width-1, height-1]);
         imwrite(im, strcat(final_directory, '/', 'cropped_', int2str(col), 'x', int2str(row),'_', files(i).name), ...
                 'Quality', 100);
