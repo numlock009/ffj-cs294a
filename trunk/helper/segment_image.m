00e5d91b6a6abfea9371d1c68594a5595d7c29f7
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-24 02:41:47 +0000

    fixed again

--- trunk/helper/segment_image.m
+++ trunk/helper/segment_image.m
@@ -3,10 +3,10 @@ files = dir(directory)
 for i= 1:size(files,1)
   if(~files(i).isdir)
     img = imread(strcat(directory, '/', files(i).name));
-    [h, w, c] = size(img);
+    [h, w, c] = size(img)
     for row =  1:height:(h-height)
       for col = 1:width:(w-width)
-        im = imcrop(img, [row, col, width-1, height-1]);
+        im = imcrop(img, [col, row, width-1, height-1]);
         imwrite(im, strcat(final_directory, '/', 'cropped_', int2str(col), 'x', int2str(row),'_', files(i).name), ...
                 'Quality', 100);
       end
