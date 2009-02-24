0392cbef648c5dd22777e9b6e62859a24deca0d8
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-24 01:30:58 +0000

    fix for segmenting of image

--- helper/segment_image.m
+++ helper/segment_image.m
@@ -2,8 +2,8 @@ function segment_image(directory, final_directory, height, width)
 files = dir(directory)
 for i= 1:size(files,1)
   if(~files(i).isdir)
-    img = imread(strcat(directory, files(i).name));
-    [h, w, c] = size(img)
+    img = imread(strcat(directory, '/', files(i).name));
+    [h, w, c] = size(img);
     for row =  1:height:(h-height)
       for col = 1:width:(w-width)
         im = imcrop(img, [col, row, height-1, width-1]);
