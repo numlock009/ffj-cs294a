f98d933f51460a2afc18ef9102cb2bf436f57c7a
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-02-25 16:00:32 +0000

    visualizing classified patches of an image

--- trunk/visualize.m
+++ trunk/visualize.m
@@ -4,6 +4,9 @@ experiment_number = '3'
 texture = 'skin_228x171'
 image_folder = '/face_segments228x171' 
 script_type = 'hr'
+desc = 'rift'
+threshold = 0.005
+
 extra = ['_', texture, '_', experiment_number, '_', script_type];
 centroid_file = ['images', image_folder, '/centroids', extra]
 paramfile = ['svm_files/svm_param', extra, '_', desc]
@@ -15,8 +18,6 @@ image_width = size(img, 2)
 image_height = size(img, 1)
 width = 228
 height = 171
-threshold = 0.005
-desc = 'rift'
 points = find_keypoints(img, 'keypt', 'hl', 'threshold', threshold);
 
 features = [];
@@ -75,14 +76,14 @@ imshow(img);
 output = reshape(output, num_cols, num_rows)';
 for i = 1:num_rows
   for j = 1:num_cols
-    if(output(i,j))
-      facecolor = 'r';
+    if(output(i,j) > 0)
+      facecolor = 'g';
     else
-      facecolor = 'b';
+      facecolor = 'r';
     end
-    t = 0:pi/2:2*pi;
-    patch((j - 1) * width + width/2 + height/2 + 1 + cos(t)*(width/sqrt(2)), (i - 1) * height + 1 + ...
-          width/2 + height/2 + sin(t)*(height/sqrt(2)),'y',...
-          'FaceAlpha', 0.1, 'FaceColor', facecolor, 'EdgeColor', facecolor);
+    c = (j - 1) * width + 1;
+    r = (i - 1) * height + 1;
+    patch([c, c + width, c + width, c], [r, r, r+height, r+height],...
+          facecolor, 'FaceAlpha', 0.2, 'EdgeColor', facecolor);
   end
 end
