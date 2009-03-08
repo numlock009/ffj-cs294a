9b3f706b2c57cc4624348e64a60a2afdb13d3778
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-03-08 01:07:25 +0000

    script for running on the cluster

--- trunk/run.sh
+++ trunk/run.sh
@@ -1,13 +1,1 @@
-expnum="5"
-text="skin_50x50"
-width=50
-height=50
-img_fol="/skin"
-st="lr"
-have_pts_train=1
-max_pts=1000
-threshold=0.005
-desc="rift"
-keypt="hl"
-cl_algo="svm"
-matlab -nodesktop -nosplash -r training_script\(\'$expnum\',\'$text\',$width,$height,\'$img_fol\',\'$st\',$have_pts_train,$max_pts,$thresh,\'$desc\',\'$keypt\',\'$cl_algo\'\)
+matlab -nodesktop -nosplash -r "full_script\(\'$expnum\',\'$text\',$width,$height,\'$img_fol\',\'$st\',$have_pts_train,$max_pts,$thresh,\'$desc\',\'$keypt\',\'$cl_algo\'\);exit"
