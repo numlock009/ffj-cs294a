2e775139cd16aad37c74299953f0f30a93b08cae
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-03-12 02:42:04 +0000

    bug in full script

--- trunk/run.sh
+++ trunk/run.sh
@@ -1,8 +1,8 @@
-output_file="results/skin_50x50_2"
+goutput_file="results/skin_50x50_junk"
 matlab -nodesktop -nosplash >> $output_file 2>&1 << EOF
-expnum='2'
+expnum='junk'
 text='skin_50x50'
-img_fol='/skin'
+img_fol='/test'
 st='lr'
 width=50
 height=50
@@ -17,6 +17,6 @@ img_dir = 'images/background'
 final_directory = [img_dir, '/fp']
 
 full_script(expnum, text, width, height, img_fol, st, ...
-            have_pts_train, max_pts, threshold, ...
+            have_pts_train, have_pts_test, max_pts, threshold, ...
             desc, keypt, cl_algo, img_dir, final_directory)
 EOF
