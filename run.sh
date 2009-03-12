a673f138fa23d48668b499a17fd6c6e7e30c1515
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-03-12 05:07:54 +0000

    added the ability to weight the svm and do regression and updated the script

--- run.sh
+++ run.sh
@@ -1,22 +1,23 @@
-output_file="results/skin_50x50_junk"
+output_file="results/new_hair_50x50_1_unweighted"
 matlab -nodesktop -nosplash >> $output_file 2>&1 << EOF
-expnum='junk'
-text='skin_50x50'
-img_fol='/test'
+expnum='1'
+text='new_hair_50x50'
+img_fol='/new_hair'
+weighted = 0
 st='lr'
 width=50
 height=50
 have_pts_train=false
 have_pts_test=false
 max_pts=1000
-threshold=0.005
+threshold=0.0005
 desc='rift'
 keypt='hl'
 cl_algo='svm'
-img_dir = 'images/background'
+img_dir = 'images/face'
 final_directory = [img_dir, '/fp']
 
 full_script(expnum, text, width, height, img_fol, st, ...
             have_pts_train, have_pts_test, max_pts, threshold, ...
-            desc, keypt, cl_algo, img_dir, final_directory)
+            desc, keypt, cl_algo, img_dir, final_directory, weighted)
 EOF
