cb678f4c28fdf45387701bd745c3adefeed6fbe4
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-03-12 02:29:46 +0000

    changes for full running to improve dataset

--- trunk/run.sh
+++ trunk/run.sh
@@ -1,21 +1,22 @@
-output_file="results/hair_50x50_spin"
-matlab -nodesktop -nosplash > $output_file 2>&1 << EOF
-expnum='1'
-text='hair_50x50'
-img_fol='/hair'
+output_file="results/skin_50x50_2"
+matlab -nodesktop -nosplash >> $output_file 2>&1 << EOF
+expnum='2'
+text='skin_50x50'
+img_fol='/skin'
 st='lr'
 width=50
 height=50
 have_pts_train=false
 have_pts_test=false
-have_
 max_pts=1000
 threshold=0.005
 desc='rift'
 keypt='hl'
 cl_algo='svm'
+img_dir = 'images/background'
+final_directory = [img_dir, '/fp']
 
 full_script(expnum, text, width, height, img_fol, st, ...
             have_pts_train, max_pts, threshold, ...
-            desc, keypt, cl_algo)
+            desc, keypt, cl_algo, img_dir, final_directory)
 EOF
