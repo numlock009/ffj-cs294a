9008529fb858d79e7c2371e4040104f979302370
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-03-18 20:38:14 +0000

    histogramming 3d fixed for edge case

--- helper/hist3d.m
+++ helper/hist3d.m
@@ -1,8 +1,12 @@
 function h = hist3d(values, binsize)
 % values is a 3d array with values between 0 and 1.
-bins = floor(1 + values*binsize);
-bins = reshape(bins, size(bins, 1) * size(bins, 2), size(bins,3));
+bins = floor(1 + abs(values-eps)*binsize);
+% bins = reshape(bins, size(bins, 1) * size(bins, 2), size(bins,3));
 h = zeros(binsize, binsize, binsize);
 for i = 1:size(bins,1)
-  h(bins(i,1), bins(i,2), bins(i, 3)) = h(bins(i,1), bins(i,2), bins(i, 3)) + 1;
+  for j = 1:size(bins, 2)
+    h(bins(i,j,1), bins(i,j,2), bins(i,j,3)) = h(bins(i,j,1), ...
+                                                 bins(i,j,2), ...
+                                                 bins(i,j,3)) + 1;
+  end
 end
