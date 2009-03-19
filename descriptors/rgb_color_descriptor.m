46614b20dd236c34bd480782c9cdcc03deeac0d2
moonpiesandbox <moonpiesandbox@987bb8ca-f7c8-11dd-a320-db7d8e7a638f>
2009-03-19 03:38:26 +0000

    color histogramming

--- descriptors/rgb_color_descriptor.m
+++ descriptors/rgb_color_descriptor.m
@@ -1,7 +1,7 @@
 function [rgb_descriptor] = rgb_color_descriptor(img_color, varargin);
 p = inputParser;
 p.KeepUnmatched = true;
-p.addRequired('img_color', @(x)size(x,3) > 2); % @ischar); % @(x)sum(size(x))>0);
+p.addRequired('img_color', @(x)true); % size(x,3) > 2); % @ischar); % @(x)sum(size(x))>0);
 p.addParamValue('width', 10, @(x)x>0);
 p.addParamValue('height', 10, @(x)x>0);
 % by default is the whole image to map color
@@ -11,6 +11,11 @@ p.parse(img_color, varargin{:});
 keypt = p.Results.pt;
 full = p.Results.full;
 
+if( size(img_color, 3) < 3 )
+  img_temp = repmat(img_color, 1, 3);
+  img_color = reshape(img_temp, size(img_color, 1), size(img_color, 2), 3);
+end
+
 if(full)
   rgb_descriptor = img_color;
 else
@@ -18,10 +23,8 @@ else
   height = p.Results.height;
   row = floor(keypt(1)/height)*height + 1;
   col = floor(keypt(2)/width)*width + 1;
-  row_end = row + height - 1;
-  col_end = col + width - 1;
-  if(row_end > size(img_color, 1)), row_end = size(img_color, 1), end
-  if(col_end > size(img_color, 2)), col_end = size(img_color, 2), end
+  row_end = min(row + height - 1, size(img_color, 1));
+  col_end = min(col + width - 1, size(img_color, 2));
   rgb_descriptor = img_color(row:row_end, col:col_end, :);
 end
 rgb_descriptor = im2double(rgb_descriptor);
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-output_file="results/old_skin_test_50x50_1_unweighted_rgb"
+output_file="results/old_skin_test_50x50_1_unweighted_hsv"
 matlab -nodesktop -nosplash >> $output_file 2>&1 << EOF
 expnum='1'
 text='skin_50x50'
@@ -12,7 +12,7 @@ keypt='hl'
 max_pts=1000
 threshold=0.0005
 desc='rift'
-feature_type='rgb'
+feature_type='hsv'
 cl_algo='svm'
 vis_dir = 'images/face'
 final_directory = [vis_dir, '/fp']
